8f42877235560bf3d3857a01c7ab2a10ca9716cf
freynoso <ingfelixreynoso2018@gmail.com>
2019-04-01 11:47:27 -0700

    Commiting the last changes that weren't pushed

--- fillit.c
+++ fillit.c
@@ -1,2 +1,21 @@
-fillit
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/uio.h>
 
+
+int    numberOfTetriminos(char **file);
+
+
+
+int     main(int argc, char **argv)
+{
+    if (argc == 2)
+    {
+        
+    }    
+    else if (argc != 2)
+        write(1, "error\n", 6);
+    return (0);
+}
